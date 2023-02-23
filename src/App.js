cbd7aa6989f838f7da4d3d6cc6282dc6d4d391ac
gaissa <jannekahkonen@gmail.com>
2023-02-23 03:59:21 +0200

    testing
    just testing the enviroment

--- src/App.js
+++ src/App.js
@@ -4,18 +4,24 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <header className="App-top-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           LFG!
         </p>
+		
+		<div className="App-content">
+		  <p>
+		    say what?
+		  </p>		  
+		</div>
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          chill...
         </a>
       </header>
     </div>
